ffd51411db6b4272e19fadd6405d1caefcfc0b56
Fernando Caamaño <fcaamano@qonico.com>
2018-10-06 10:14:41 -0300

    Create a simple service for getting launches and a specific launch using observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LaunchService } from './launch.service';
 
 @Component({
   selector: 'lh-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'launch-hub';
+
+  constructor(private launch: LaunchService){
+    this.launch.getLaunches().subscribe((data) => {
+      console.log(data);
+      console.log(data[0]);
+    });
+
+    this.launch.getLaunch(3).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
